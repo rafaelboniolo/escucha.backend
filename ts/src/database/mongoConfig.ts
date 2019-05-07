c3a174bc162ffdc2f96b493f93733e3426e5db6b
Rafael Boniolo <33812322+rafaelboniolo@users.noreply.github.com>
2019-05-07 11:41:34 -0300

    Update mongoConfig.ts

--- ts/src/database/mongoConfig.ts
+++ ts/src/database/mongoConfig.ts
@@ -1,5 +1,5 @@
 const MONGO_USER="boniolo"
-const MONGO_PASSWORD="cdanlj78"
+const MONGO_PASSWORD="********"
 const MONGO_URL="cluster0-fndvs.mongodb.net/test?retryWrites=true"
 
 export default "mongodb+srv://"+MONGO_USER+":"+MONGO_PASSWORD+"@"+MONGO_URL
