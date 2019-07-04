020a9764ccc655a6f9c956b298d3055144d803e4
Rafael Boniolo <33812322+rafaelboniolo@users.noreply.github.com>
2019-07-04 01:03:33 -0300

    add info logs

--- ts/src/server.ts
+++ ts/src/server.ts
@@ -1,5 +1,8 @@
+import Infos from "./util/Infos";
 import app from "./app";
-
+Infos.showInit();
+Infos.showTeam();
 app.listen(3000);
+Infos.showListen();
 
 
---
+++ ts/src/util/Infos.ts
@@ -0,0 +1,20 @@
+export default class Infos{
+    static showTeam(){
+        console.log("********************************");
+        console.log("****Escucha Development Team****");
+        console.log("********************************");
+        console.log("************GO HORSE************");
+        console.log("********************************\n\n");       
+    }
+    static showInit(){
+        console.log("********************************");
+        console.log("******Initializing Service******");
+        console.log("********************************\n\n");       
+    }
+
+    static showListen(){
+        console.log("********************************");
+        console.log("*****Listening on port 3000*****");
+        console.log("********************************\n\n");       
+    }
+}
